ad287532a80a2ad6c5d62fc8d6f5e63349b231bd
Kapil <kaps146singh@gmail.com>
2023-08-27 16:40:19 +0530

    final commit

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+import project from '../assets/projects/project-one.png'
+export const data=[
+    {
+        id:1,
+        name:"PenTrack(Under development)",
+        image:project,
+        github:"https://github.com/KapilSinghDev/Cyber-Checklist.git",
+        live:"https://cyber-checklist.vercel.app/",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+
+
+]
